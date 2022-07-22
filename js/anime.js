d49d21767c36759788d18b6e692101f748264707
Alejandro Camacho <alejandro.camacho.olivares@gmail.com>
2022-07-22 14:51:41 -0500

    Input search anime done. Problems with anime info.

--- js/anime.js
+++ js/anime.js
@@ -3,9 +3,15 @@ const API_PATH = "https://kitsu.io/api/edge"
 const content_to_listen = document.getElementById("anime-name")
 content_to_listen.addEventListener("keypress", data => {
     if(data.key === "Enter"){
+        resizeInput()
         getName()
     }
 })
+
+function resizeInput(){
+    let input_anime = document.getElementById("search-anime")
+    console.log(input_anime.className = "searching-on")
+}
 
 function getName(){
     const content = document.getElementById("anime-name")
@@ -42,40 +48,76 @@ function clean_section_anime(anime_section){
 }
 
 function create_elements(data, anime_section){
+    let anime_section_info = document.createElement("section")
+    anime_section_info.className = "anime-section-info"
+
+    let div_title = document.createElement("div")
+    div_title.className = "div-title"
+
     let anime_title = document.createElement("h2")
     anime_title.innerHTML = `${data.data[0].attributes.titles.en_jp}`
+    
+    let div_subtitle = document.createElement("div")
+    div_subtitle.className = "div-subtitle"
+
     let anime_title_jap = document.createElement("h3")
     anime_title_jap.innerHTML = `${data.data[0].attributes.titles.ja_jp}`
+    
+    let container_img = document.createElement("figure")
+    
     let anime_image = document.createElement("img")
+    anime_image.className = "anime_cover"
     anime_image.src = `${data.data[0].attributes.posterImage.large}`
+
+
     let anime_synopsis = document.createElement("p")
+    anime_synopsis.className = "anime-synopsis"
     anime_synopsis.innerHTML = `${data.data[0].attributes.synopsis}`
+
+    let div_data = document.createElement("div")
+    div_data.className = "div-data"
+
     let anime_total_episodes = document.createElement("p")
     anime_total_episodes.innerHTML = `Total Episodes: ${data.data[0].attributes.episodeCount}`
+    
     let anime_total_duration = document.createElement("p")
     anime_total_duration.innerHTML = `Episode Duration: ${data.data[0].attributes.episodeLength} min`
+    
     let anime_start_date = document.createElement("p")
     anime_start_date.innerHTML = `Start date: ${data.data[0].attributes.startDate}`
+    
     let anime_end_date = document.createElement("p")
     anime_end_date.innerHTML = `End date: ${data.data[0].attributes.endDate}`
+    
     let anime_show_type = document.createElement("p")
     anime_show_type.innerHTML = `Show Type: ${data.data[0].attributes.showType}`
+    
     let anime_nsfw = document.createElement("p")
     anime_nsfw.innerHTML = `NSFW: ${nsfw_anime_status(data)}`
+    
     let anime_status = document.createElement("p")
     anime_status.innerHTML = `Status: ${data.data[0].attributes.status}`
 
-    anime_section.appendChild(anime_image)
-    anime_section.appendChild(anime_title)
-    anime_section.appendChild(anime_title_jap)
-    anime_section.appendChild(anime_synopsis)
-    anime_section.appendChild(anime_total_episodes)
-    anime_section.appendChild(anime_total_duration)
-    anime_section.appendChild(anime_start_date)
-    anime_section.appendChild(anime_end_date)
-    anime_section.appendChild(anime_show_type)
-    anime_section.appendChild(anime_nsfw)
-    anime_section.appendChild(anime_status)
+    // anime section
+    //     container img
+    //     anime_section_info
+    anime_section.appendChild(container_img)
+    container_img.appendChild(anime_image)
+
+    anime_section.appendChild(anime_section_info)
+    anime_section_info.appendChild(div_title)
+    div_title.appendChild(anime_title)
+    anime_section_info.appendChild(div_subtitle)
+    div_subtitle.appendChild(anime_title_jap)
+    anime_section_info.appendChild(anime_synopsis)
+    anime_section_info.appendChild(div_data)
+    div_data.appendChild(anime_total_episodes)
+    div_data.appendChild(anime_total_duration)
+    div_data.appendChild(anime_start_date)
+    div_data.appendChild(anime_end_date)
+    div_data.appendChild(anime_show_type)
+    div_data.appendChild(anime_nsfw)
+    div_data.appendChild(anime_status)
 }
 
 function nsfw_anime_status(data){
